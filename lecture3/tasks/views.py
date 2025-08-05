30bfe95debdc9b653c1d72e059b0a5eab290cc0b
saqib <saqibkhattak16@gmail.com>
2025-08-05 14:06:52 +0500

    session add to todo list app

--- lecture3/tasks/views.py
+++ lecture3/tasks/views.py
@@ -3,7 +3,6 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-tasks = []
 
 class NewtaskForm(forms.Form):
     task = forms.CharField(label="New Task")
@@ -12,8 +11,11 @@ class NewtaskForm(forms.Form):
 
 # Create your views here.
 def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
+
     return render( request, "tasks/index.html", { 
-        "tasks" : tasks
+        "tasks" : request.session["tasks"]
     } )
 
 def add(request):
@@ -21,7 +23,7 @@ def add(request):
         form = NewtaskForm(request.POST)
         if form.is_valid():
             task = form.cleaned_data["task"]
-            tasks.append(task)
+            request.session["tasks"] += [task]
             return HttpResponseRedirect(reverse("tasks:index"))
         else: 
             return render(request, "tasks/add.html",{
